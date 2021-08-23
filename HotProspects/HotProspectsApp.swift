f3c27809cd625cf10e3ad1c3ef93f060af02026a
Olivier Van hamme <nicefiction@gmail.com>
2021-08-23 09:23:19 +0200

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Olivier Van hamme on 23/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
